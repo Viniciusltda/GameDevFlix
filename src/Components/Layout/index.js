fc251ef0b4bd9d1672aa6bdf407863589665954e
Vinícius Alves Mesquita <vinizmesquita@gmail.com>
2020-07-29 00:56:10 -0300

    Scroll do Carousel

--- src/Components/Layout/index.js
+++ src/Components/Layout/index.js
@@ -1,33 +1,28 @@
 import React from 'react';
 import Menu from '../Menus';
 import Footer from '../Footer';
-import Styled from 'styled-components';
+import styled from 'styled-components';
 
-const Main = Styled.main`
-    background-color: var(--black);
-    color: var(--white);
+const Main = styled.main`
+  background-color: var(--black);
+  color: var(--white);
+  flex: 1;
+  padding-top: 50px;
+  padding-left: 5%;
+  padding-right: 5%;
+`;
 
-    flex: 1;
 
-    padding-top: 50px;
-    padding-left: 5%;
-    padding-right: 5%;
-`
-
-function DefaultPage({children}){
-    return (
-        <>
-            <Menu/>
-
-                <Main>
-                    {children}
-
-                </Main>
-
-            <Footer/>
-
-        </>
-    );
+function DefaultPage({ children }) {
+  return (
+    <>
+      <Menu />
+        <Main>
+          {children}
+        </Main>
+      <Footer />
+    </>
+  );
 }
 
 export default DefaultPage;
